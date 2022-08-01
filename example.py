1eaf864cc460045e9d9745b848c7ecfaa7dabd90
Pim Merks <pimmerks@users.noreply.github.com>
2022-08-01 11:11:29 +0200

    Updated other references to oplaadpalen

--- example.py
+++ example.py
@@ -3,7 +3,7 @@ from eco_movement_api.models import Status
 
 client = EcoMovementClient()
 
-details = client.get_location_details('3771ff27e884611ebba6042010a84000')
+details = client.get_location_details('670a6ba993011e9949442010a8400035')
 
 print(details)
 print(details.data.evses[0].status == Status.AVAILABLE)
